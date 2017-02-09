fcf2bff9d3440a187dbd288ae224f31e5fc76b75
Windy Wang <windy.znw@gmail.com>
2017-02-09 13:02:08 -0500

    Added scatterplot and included facet/line options

--- R/graphs.R
+++ R/graphs.R
@@ -1,7 +1,11 @@
+
 # Boxplot -------------------------------
-box_plot <- function(data, xvar, yvar, xlab="", ylab="") {
-  ggplot2::ggplot(data, ggplot2::aes_string(x = xvar, y = yvar)) +
-    ggplot2::geom_boxplot(ggplot2::aes_string(colour = xvar, fill = xvar)) +
+
+box_plot <- function(data, xvar, yvar, xlab="", ylab="", facet = FALSE) {
+  myboxplot <- ggplot2::ggplot(data, ggplot2::aes_string(x = xvar, y = yvar)) +
+    ggplot2::geom_jitter(ggplot2::aes_string(colour = xvar, alpha = 0.4), width = 0.33) +
+    ggplot2::geom_boxplot(ggplot2::aes_string(colour = xvar, fill = xvar), 
+                          outlier.shape = NA) +
     ggplot2::stat_summary(geom = "crossbar", width = 0.65, fatten = 0, color = "white",
                           fun.data = function(x){
                             return(c(y = median(x), ymin = median(x), ymax = median(x)))
@@ -16,18 +20,93 @@ box_plot <- function(data, xvar, yvar, xlab="", ylab="") {
                    axis.title = ggplot2::element_text(size = 10)) +
     ggplot2::xlab(xlab) +
     ggplot2::ylab(ylab)
+  if(facet == TRUE) {
+    myboxplot +
+      ggplot2::facet_grid(~fVN, switch = "x")
+  } else {
+    myboxplot
+  }
 }
 
-# scale_x_discrete(labels = paste(levels(ds_base$xvar), 
-#                                 "\n(N=", table(ds_base$xvar), ")", sep = "")) +
+# Scatterplot -------------------------------------------
+
+scatter_plot = function(data, xvar, yvar, xlab='', ylab='', line = TRUE, facet = FALSE) {
+  myplot <- ggplot2::ggplot(data, ggplot2::aes_string(x=xvar, y=yvar)) +
+    ggplot2::geom_point(colour = "#0db7c4", size = 1) + #mapping=aes(color=mcr_status)
+    ggplot2::theme_minimal() + 
+    ggplot2::theme(panel.grid.major.x = ggplot2::element_blank(), 
+                   axis.line.y = ggplot2::element_blank(),
+                   axis.text.y = ggplot2::element_text(colour = "grey"),
+                   axis.ticks.y = ggplot2::element_line(colour = "grey"),
+                   axis.text.x = ggplot2::element_text(colour = "grey50"), #angle = 45
+                   axis.title = ggplot2::element_text(size = 10)) +
+    ggplot2::xlab(xlab) +
+    ggplot2::ylab(ylab)
+  if(line == TRUE & facet == FALSE) {
+    myplot +
+      ggplot2::geom_smooth(method = lm, colour = "grey50")
+  } else if(line == TRUE & facet == TRUE) {
+    myplot +
+      ggplot2::geom_smooth(method = lm, colour = "grey50") +
+      ggplot2::facet_grid(~fVN, switch = "x")
+  } else if(line == FALSE & facet == TRUE) {
+    myplot +
+      ggplot2::facet_grid(~fVN, switch = "x")
+  } else {
+    myplot
+  }
+}
+
+# Histogram -----------------------------------------------
+
+histo_plot = function(data, variable, bin, xlab='', facet = FALSE) {
+  histoplot <- ggplot2::ggplot(data, ggplot2::aes_string(x=variable)) +
+    ggplot2::geom_histogram(binwidth=bin,
+                            colour='#0db7c4', fill='#0db7c4') +
+    ggplot2::xlab(xlab) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(panel.grid.major.x = ggplot2::element_blank(), 
+                   axis.line.y = ggplot2::element_blank(),
+                   axis.text.y = ggplot2::element_text(colour = "grey"),
+                   axis.ticks.y = ggplot2::element_line(colour = "grey"),
+                   axis.text.x = ggplot2::element_text(colour = "grey 30"), #angle = 45
+                   axis.title = ggplot2::element_text(size = 10))
+  if(facet == TRUE) {
+    histoplot + 
+      ggplot2::facet_grid(~fVN, switch = "x")
+  } else {
+    histoplot
+  }
+}
 
 
-
-# box_plot = function(data, xvar, yvar, xlab='', ylab='') {
-#   ggplot(data, aes(x=xvar, y=yvar)) +
-#     geom_boxplot() +
-#     # scale_x_discrete(limits=xcat) +
-#     xlab(xlab) +
-#     ylab(ylab) +
-#     theme_bw()
-# }
+#' Line Plot
+#'
+#' @param data Dataset (cleaned) 
+#' @param xvar Variable on x axis (continous or factor)
+#' @param yvar Variable on y axis (continuous or factor)
+#' @param xlab Axis label for x axis
+#' @param ylab Axis label for y axis
+#'
+#' @return Plot
+#'
+#' @examples
+#' ds %>% 
+#'   dplyr::filter(SID < 1050 ) %>% 
+#'   line_plot("fVN", "UDBP", "SID", "Visit Number", "UDBP")
+line_plot = function(data, xvar, yvar, byvar, xlab='', ylab='') {
+  ggplot2::ggplot(data, ggplot2::aes_string(x = xvar, y = yvar, 
+                                            group = byvar, colour = byvar)) +
+    ggplot2::geom_line() +
+    ggplot2::geom_point() +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(legend.position = "none",
+                   panel.grid.major.x = ggplot2::element_blank(), 
+                   axis.line.y = ggplot2::element_blank(),
+                   axis.text.y = ggplot2::element_text(colour = "grey"),
+                   axis.ticks.y = ggplot2::element_line(colour = "grey"),
+                   axis.text.x = ggplot2::element_text(colour = "grey 30"), #angle = 45
+                   axis.title = ggplot2::element_text(size = 10)) +
+    ggplot2::xlab(xlab) +
+    ggplot2::ylab(ylab)
+}
